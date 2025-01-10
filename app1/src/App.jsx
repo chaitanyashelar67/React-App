aabbe3591d8473badb1dc1f9d4715e39d78541fe
chaitanyashelar67 <chaitanyashelar67@gmail.com>
2025-01-10 14:51:33 +0530

    done

--- app1/src/App.jsx
+++ app1/src/App.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import "./App.css";
+import "./style.css";
 import { Header } from "./Components/header/header";
+
 
 
 
 const App = () => {
   return (
     <>
+     <Header />
       <BrowserRouter>
-        <Header />
         <Routes>
         <Route path="/" element={<Home />} />
         </Routes>
--- app1/src/Components/new-arrivals/NewArrivals.jsx
+++ app1/src/Components/new-arrivals/NewArrivals.jsx
@@ -1,38 +1,52 @@
-import React, { useEffect, useState } from 'react'
-import "./newArrivals.css"
+import { useEffect, useState } from 'react';
+import "./newArrivals.css";
 
 const NewArrivals = () => {
-  const[products, setProduct] =useState([]);
+  const [products, setProduct] = useState([]);
 
   async function getProducts() {
     try {
-      const res = await fetch ("https://dummyjson.com/products")
-    const data = await res.json();
-
-    setProducts(data.products);
+      const res = await fetch("https://dummyjson.com/products");
+      const data = await res.json();
+      setProduct(data.products);
+      console.log(data);
     } catch (error) {
       console.log(error);
     }
   }
-  useEffect (()=>{ 
-    getProduct();
-  },[])
-  return (
-  <>
-  <section class="cards-container">
-        <div class="card-heading">
-            <h2>NEW ARRIVALS</h2>
-        </div>
-        <div class="card-item flex center wrap">
-           
-        </div>
-        
-        <div class="view-all-btn flex center">
-            <button class="show-all">View All</button>
-        </div>
-    </section>
-  </>
-  )
-}
 
-export default NewArrivals
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  return (
+    <div className="new-arrivals-section">
+      <div className="card-heading">
+        <h2>NEW ARRIVALS</h2>
+      </div>
+      <div className="cards-container">
+        <div className="card-item">
+          {products.map((product) => (
+            <div key={product.id} className="card">
+              <div className="item-img">
+                <img src={product.thumbnail} alt={product.title} />
+              </div>
+              <div className="item-text">
+                {/* <h3>{product.title}</h3> */}
+                {/* <p>{product.description}</p> */}
+                {/* <div className="pricing">
+                  // <p>${product.price}</p>
+                </div> */}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="view-all-btn flex center">
+        <button className="show-all">View All</button>
+      </div>
+    </div>
+  );
+};
+
+export default NewArrivals;
